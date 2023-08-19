02643b205d5884202a70af42a65b2ac1e6b2fdd6
Shrijay <gauns2001@gmail.com>
2023-08-19 12:25:54 +0530

    First commit throuh vs

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../common/Footer";
+
+function Home() {
+
+
+	return (
+		<div className="container">
+			<h1>MERN Stack CRUD</h1>
+			<p>
+				This is a basic MERN Application
+			</p>
+			<Footer />
+		</div>
+	);
+}
+
+export default Home;
